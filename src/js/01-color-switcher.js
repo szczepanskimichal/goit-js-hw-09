ac0e7e799a01fe820d517f824e0f308d8ca99aa8
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-09-04 12:26:23 +0200

    start
    added support for the "Start" button

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,3 +5,15 @@ let randomColorBg;
 function getRandomColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+//START
+startBtnEl.addEventListener(
+  'click',
+  () => {
+    document.body.style.backgroundColor = getRandomColor();
+    console.log(getRandomColor());
+    startBtnEl.disabled = true;
+    stopBtnEl.disabled = false;
+  },
+  1000
+);
