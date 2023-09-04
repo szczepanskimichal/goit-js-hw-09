81e7f9241aa0916d508f05c04773fe0783d51821
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-09-04 12:50:27 +0200

    w
    q

---
+++ src/js/02-timer.js
@@ -0,0 +1,11 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const startEl = document.querySelector('[data-start]');
+const dataPickerEl = document.getElementById('datetime-picker');
+const daysEl = document.querySelector('[data-days]');
+const hoursEl = document.querySelector('[data-hours]');
+const minutesEl = document.querySelector('[data-minutes]');
+const secondsEl = document.querySelector('[data-seconds]');
+let currentDate;
