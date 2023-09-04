80c64cd362e34b9318af5b5ec8bedc85b80a7d70
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-09-04 10:26:55 +0000

    Deploying to gh-pages from @ szczepanskimichal/goit-js-hw-09@ac0e7e799a01fe820d517f824e0f308d8ca99aa8 🚀

---
+++ 01-color-switcher.97b0af2e.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start"),o=document.querySelector("[data-stop");function e(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}t.addEventListener("click",(function(){document.body.style.backgroundColor=e(),console.log(e()),t.disabled=!0,o.disabled=!1}),1e3)}();
+//# sourceMappingURL=01-color-switcher.97b0af2e.js.map
