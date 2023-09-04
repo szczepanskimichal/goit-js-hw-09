b89864bcbb85ab19d0d69d9f0f92d1f3283070f0
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-09-04 10:30:38 +0000

    Deploying to gh-pages from @ szczepanskimichal/goit-js-hw-09@54e21f28ee8579a2520cb90466af7c8cf0dff2ea 🚀

--- 01-color-switcher.05369c14.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("[data-start"),o=document.querySelector("[data-stop");function e(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(()=>{document.body.style.backgroundColor=e(),console.log(e()),t.disabled=!0,o.disabled=!1}),1e3);
-//# sourceMappingURL=01-color-switcher.05369c14.js.map
---
+++ 01-color-switcher.41d72819.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start"),t=document.querySelector("[data-stop");function d(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}e.addEventListener("click",(()=>{document.body.style.backgroundColor=d(),console.log(d()),e.disabled=!0,t.disabled=!1}),1e3),t.addEventListener("click",(()=>{clearInterval(undefined),console.log("stopped"),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.41d72819.js.map
