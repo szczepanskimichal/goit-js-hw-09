ec881f9f3dc4bf111ad20585e2e0c33e2ed07a22
szczepanskimichal <127307935+szczepanskimichal@users.noreply.github.com>
2023-09-04 12:21:03 +0200

    w
    q

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,7 @@
+const startBtnEl = document.querySelector('[data-start');
+const stopBtnEl = document.querySelector('[data-stop');
+let randomColorBg;
 
+function getRandomColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
